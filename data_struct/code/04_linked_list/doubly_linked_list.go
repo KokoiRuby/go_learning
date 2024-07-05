4163132a914033b15f46903132749b66ec218518
Kokoi Ruby <kokoiruby@gmail.com>
2024-07-05 14:52:34 +0800

    ++doubly_linked_list

--- data_struct/code/04_linked_list/doubly_linked_list.go
+++ data_struct/code/04_linked_list/doubly_linked_list.go
@@ -1,1 +1,126 @@
 package _4_linked_list
+
+import (
+	"errors"
+	"fmt"
+)
+
+type NodeD struct {
+	prev *NodeD
+	next *NodeD
+	val  interface{}
+}
+
+type DoublyLinkedList struct {
+	head   *NodeD
+	length uint
+}
+
+func NewNodeD(v interface{}) *NodeD {
+	return &NodeD{
+		val: v,
+	}
+}
+
+func NewDoublyLinkedList() *DoublyLinkedList {
+	return &DoublyLinkedList{
+		head:   NewNodeD(nil),
+		length: 0,
+	}
+}
+
+func (dll *DoublyLinkedList) GetHead() *NodeD {
+	return dll.head
+}
+
+func (dll *DoublyLinkedList) GetTail() *NodeD {
+	cur := dll.head
+	for cur.next != nil {
+		cur = cur.next
+	}
+	return cur
+}
+
+func (dll *DoublyLinkedList) Append(v interface{}) {
+	if dll.head.next == nil {
+		dll.head.next = NewNodeD(v)
+	} else {
+		tail := dll.GetTail()
+		newNode := NewNodeD(v)
+		tail.next = newNode
+		newNode.prev = tail
+	}
+	dll.length++
+}
+
+func (dll *DoublyLinkedList) FindByIndex(idx uint) (*NodeD, error) {
+	if idx >= dll.length {
+		return nil, errors.New("out of range")
+	}
+	cur := dll.head.next
+	for i := uint(0); i < idx; i++ {
+		cur = cur.next
+	}
+	return cur, nil
+}
+
+func (dll *DoublyLinkedList) InsertAfter(idx uint, v interface{}) (bool, error) {
+	if idx >= dll.length {
+		return false, errors.New("out of range")
+	}
+	cur, _ := dll.FindByIndex(idx)
+	newNode := NewNodeD(v)
+
+	newNode.next = cur.next
+	cur.next.prev = newNode
+	cur.next = newNode
+	newNode.prev = cur
+
+	dll.length++
+	return true, nil
+}
+
+func (dll *DoublyLinkedList) InsertBefore(idx uint, v interface{}) (bool, error) {
+	if idx >= dll.length {
+		return false, errors.New("out of range")
+	}
+	cur, _ := dll.FindByIndex(idx)
+	newNode := NewNodeD(v)
+
+	cur.prev.next = newNode
+	newNode.prev = cur.prev
+	newNode.next = cur
+	cur.prev = newNode
+
+	dll.length++
+	return true, nil
+
+}
+
+func (dll *DoublyLinkedList) Remove(idx uint) interface{} {
+	if idx >= dll.length {
+		return nil
+	}
+	cur, _ := dll.FindByIndex(idx)
+
+	v := cur.val
+	cur.prev.next = cur.next
+	cur.next.prev = cur.prev
+	cur = nil
+
+	dll.length--
+	return v
+}
+
+func (dll *DoublyLinkedList) Print() {
+	cur := dll.head.next
+	format := ""
+	for cur != nil {
+		format += fmt.Sprintf("%+v", cur.val)
+		cur = cur.next
+		if cur != nil {
+			format += "->"
+		}
+	}
+	fmt.Println(format)
+}
--- data_struct/code/04_linked_list/doubly_linked_list_test.go
+++ data_struct/code/04_linked_list/doubly_linked_list_test.go
@@ -1,1 +1,48 @@
 package _4_linked_list
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type DoublyLinkedListTestSuite struct {
+	suite.Suite
+	dll *DoublyLinkedList
+}
+
+func (suite *DoublyLinkedListTestSuite) SetupTest() {
+	suite.dll = NewDoublyLinkedList()
+	for i := 0; i < 5; i++ {
+		suite.dll.Append(i)
+	}
+	suite.dll.Print()
+}
+
+func (suite *DoublyLinkedListTestSuite) TestInsertAfter() {
+	ok, _ := suite.dll.InsertAfter(3, 33)
+	assert.Equal(suite.T(), true, ok)
+	suite.dll.Print()
+
+	_, err := suite.dll.InsertAfter(100, 1000)
+	assert.EqualError(suite.T(), err, "out of range")
+}
+
+func (suite *DoublyLinkedListTestSuite) TestInsertBefore() {
+	ok, _ := suite.dll.InsertBefore(3, 33)
+	assert.Equal(suite.T(), true, ok)
+	suite.dll.Print()
+
+	_, err := suite.dll.InsertBefore(100, 1000)
+	assert.EqualError(suite.T(), err, "out of range")
+}
+
+func (suite *DoublyLinkedListTestSuite) TestRemove() {
+	v := suite.dll.Remove(3)
+	assert.Equal(suite.T(), 3, v)
+	suite.dll.Print()
+}
+
+func TestDoublyLinkedListTestSuite(t *testing.T) {
+	suite.Run(t, new(DoublyLinkedListTestSuite))
+}
