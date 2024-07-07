ec598c60d69281ad2b5f232e848e6dd9fcf0c08a
Kokoi Ruby <kokoiruby@gmail.com>
2024-07-07 22:24:25 +0800

    array queue

--- data_struct/code/06_queue/queue_by_array.go
+++ data_struct/code/06_queue/queue_by_array.go
@@ -1,1 +1,60 @@
 package _6_queue
+
+import (
+	"errors"
+	"fmt"
+)
+
+type ArrayQueue struct {
+	q    []interface{}
+	head int
+	tail int
+	size int
+}
+
+func NewArrayQueue(capacity int) *ArrayQueue {
+	return &ArrayQueue{
+		q:    make([]interface{}, capacity),
+		head: 0,
+		tail: 0,
+	}
+}
+
+func (q *ArrayQueue) Enqueue(v interface{}) error {
+	// reach the end of array → migrate
+	if q.tail == cap(q.q) && q.head != 0 {
+		for i := q.head; i < q.tail; i++ {
+			q.q[i-q.head] = q.q[i]
+		}
+		q.tail = q.tail - q.head
+		q.head = 0
+
+	}
+	if q.size == cap(q.q) {
+		return errors.New("queue is full")
+	}
+	q.q[q.tail] = v
+	q.tail++
+	q.size++
+	return nil
+}
+
+func (q *ArrayQueue) Dequeue() (interface{}, error) {
+	if q.IsEmpty() {
+		return nil, errors.New("queue is empty")
+	}
+	v := q.q[q.head]
+	q.head++
+	q.size--
+	return v, nil
+}
+
+func (q *ArrayQueue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q ArrayQueue) Print() {
+	for i := 0; i < cap(q.q); i++ {
+		fmt.Println(q.q[i])
+	}
+}
--- data_struct/code/06_queue/queue_by_array_test.go
+++ data_struct/code/06_queue/queue_by_array_test.go
@@ -1,1 +1,44 @@
 package _6_queue
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ArrayQueueTestSuite struct {
+	suite.Suite
+	q *ArrayQueue
+}
+
+func (suite *ArrayQueueTestSuite) SetupTest() {
+	suite.q = NewArrayQueue(5)
+	for i := 0; i < 5; i++ {
+		err := suite.q.Enqueue(i)
+		assert.Nil(suite.T(), err)
+	}
+}
+
+func (suite *ArrayQueueTestSuite) TestEnqueue() {
+	err := suite.q.Enqueue(5)
+	assert.EqualError(suite.T(), err, "queue is full")
+
+	v, _ := suite.q.Dequeue()
+	assert.Equal(suite.T(), 0, v)
+
+	err = suite.q.Enqueue(5)
+	assert.Nil(suite.T(), err)
+}
+
+func (suite *ArrayQueueTestSuite) TestDequeue() {
+	for i := 0; i < 5; i++ {
+		v, _ := suite.q.Dequeue()
+		assert.Equal(suite.T(), i, v)
+	}
+	_, err := suite.q.Dequeue()
+	assert.EqualError(suite.T(), err, "queue is empty")
+}
+
+func TestArrayQueueTestSuite(t *testing.T) {
+	suite.Run(t, new(ArrayQueueTestSuite))
+}
