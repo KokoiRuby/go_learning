82b1f0c805d5357e5c240093a627cf021bc33a86
Kokoi Ruby <kokoiruby@gmail.com>
2024-07-08 20:12:44 +0800

    linked list queue

--- data_struct/code/06_queue/queue_by_linked_list.go
+++ data_struct/code/06_queue/queue_by_linked_list.go
@@ -1,1 +1,67 @@
 package _6_queue
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Node struct {
+	val  interface{}
+	next *Node
+}
+
+type LinkedListQueue struct {
+	head     *Node
+	tail     *Node
+	size     int
+	capacity int
+}
+
+func NewLinkedListQueue(capacity int) *LinkedListQueue {
+	return &LinkedListQueue{
+		head:     nil,
+		tail:     nil,
+		size:     0,
+		capacity: capacity,
+	}
+}
+
+func (q *LinkedListQueue) Enqueue(v interface{}) error {
+	if q.size == q.capacity {
+		return errors.New("queue is full")
+	}
+	node := &Node{v, nil}
+	if q.IsEmpty() {
+		q.tail = node
+		q.head = node
+	} else {
+		q.tail.next = node
+		q.tail = node
+	}
+	q.size++
+	return nil
+}
+
+func (q *LinkedListQueue) Dequeue() (interface{}, error) {
+	if q.IsEmpty() {
+		return nil, errors.New("queue is empty")
+	}
+	v := q.head.val
+	q.head = q.head.next
+	q.size--
+	return v, nil
+}
+
+func (q *LinkedListQueue) IsEmpty() bool {
+	return q.size == 0
+}
+
+func (q *LinkedListQueue) Print() error {
+	if q.IsEmpty() {
+		return errors.New("queue is empty")
+	}
+	for cur := q.head; cur != nil; cur = cur.next {
+		fmt.Printf("%v → ", cur.val)
+	}
+	return nil
+}
--- data_struct/code/06_queue/queue_by_linked_list_test.go
+++ data_struct/code/06_queue/queue_by_linked_list_test.go
@@ -1,1 +1,51 @@
 package _6_queue
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type LinkedListQueueTestSuite struct {
+	suite.Suite
+	q *LinkedListQueue
+}
+
+func (suite *LinkedListQueueTestSuite) SetupTest() {
+	suite.q = NewLinkedListQueue(5)
+	for i := 0; i < 5; i++ {
+		err := suite.q.Enqueue(i)
+		assert.Nil(suite.T(), err)
+	}
+	err := suite.q.Print()
+	assert.Nil(suite.T(), err)
+	suite.T().Logf("\n")
+}
+
+func (suite *LinkedListQueueTestSuite) TestEnqueue() {
+	err := suite.q.Enqueue(5)
+	assert.EqualError(suite.T(), err, "queue is full")
+
+	v, _ := suite.q.Dequeue()
+	assert.Equal(suite.T(), 0, v)
+
+	err = suite.q.Enqueue(5)
+	assert.Nil(suite.T(), err)
+
+	err = suite.q.Print()
+	assert.Nil(suite.T(), err)
+	suite.T().Logf("\n")
+}
+
+func (suite *LinkedListQueueTestSuite) TestDequeue() {
+	for i := 0; i < 5; i++ {
+		v, _ := suite.q.Dequeue()
+		assert.Equal(suite.T(), i, v)
+	}
+	_, err := suite.q.Dequeue()
+	assert.EqualError(suite.T(), err, "queue is empty")
+}
+
+func TestLinkedListQueueTestSuite(t *testing.T) {
+	suite.Run(t, new(LinkedListQueueTestSuite))
+}
