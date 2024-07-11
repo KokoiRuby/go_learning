e7b82ff4dcb98132442a65663bb85bfce97e669f
Kokoi Ruby <kokoiruby@gmail.com>
2024-07-11 21:17:46 +0800

    bst

--- data_struct/code/12_tree/bin_search_tree.go
+++ data_struct/code/12_tree/bin_search_tree.go
@@ -8,14 +8,125 @@ func NewBST(root *Node) *BST {
 	return &BST{NewBinaryTree(root)}
 }
 
-func (bst *BST) Find(v interface{}) *Node {
-	return nil
+func (bst *BST) Search(v interface{}) (found *Node, isLeftOf bool, parent *Node) {
+	var p *Node
+	node := bst.GetRoot()
+	var isLeft bool
+
+	for node != nil {
+		nodeVal := node.v
+		// base case
+		if nodeVal == v {
+			if larger(p, node) {
+				isLeft = true
+				return p, isLeft, node
+			}
+			isLeft = false
+			return p, isLeft, node
+		} else if larger(nodeVal, v) {
+			// recursive case
+			p = node
+			node = node.right
+		} else {
+			// recursive case
+			p = node
+			node = node.left
+		}
+	}
+	return nil, false, nil
 }
 
 func (bst *BST) Insert(v interface{}) bool {
-	return false
+	node := bst.GetRoot()
+	for node != nil {
+		nodeVal := node.v
+		if nodeVal == v {
+			return false
+		} else if larger(nodeVal, v) {
+			// base case
+			if node.right != nil {
+				node.right = NewNode(v)
+				return true
+			}
+			// recursive case
+			node = node.right
+		} else {
+			// base case
+			if node.left != nil {
+				node.left = NewNode(v)
+				return true
+			}
+			// recursive case
+			node = node.left
+		}
+	}
+	return true
 }
 
 func (bst *BST) Remove(v interface{}) bool {
+	found, isLeftOf, parent := bst.Search(v)
+	if found == nil {
+		return false
+	}
+	// no child
+	if found.left == nil && found.right == nil {
+		found = nil
+		return true
+	} else if found.right == nil && found.left != nil {
+		// 1 child
+		if isLeftOf {
+			parent.left = found.left
+			found = nil
+			return true
+		} else {
+			parent.right = found.left
+			found = nil
+			return true
+		}
+	} else if found.left == nil && found.right != nil {
+		// 1 child
+		if isLeftOf {
+			parent.left = found.right
+			found = nil
+			return true
+		} else {
+			parent.right = found.right
+			found = nil
+			return true
+		}
+	} else {
+		// 2 children
+		tmp := NewBST(found.right)
+		minimum := tmp.Min()
+		found.v = minimum.v
+		minimum = nil
+	}
 	return false
+}
+
+func larger(a, b interface{}) bool {
+	aInt := a.(int)
+	bInt := b.(int)
+
+	if aInt > bInt {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (bst *BST) Min() *Node {
+	node := bst.GetRoot()
+	for node != nil {
+		node = node.left
+	}
+	return node
+}
+
+func (bst *BST) Max() *Node {
+	node := bst.GetRoot()
+	for node != nil {
+		node = node.right
+	}
+	return node
 }
--- data_struct/code/12_tree/bin_search_tree_test.go
+++ data_struct/code/12_tree/bin_search_tree_test.go
@@ -1,16 +1,104 @@
 package _12_tree
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
 type BSTTestSuite struct {
 	suite.Suite
-	t *BST
+	bst *BST
 }
 
 func (s *BSTTestSuite) SetupTest() {
+	s.bst = NewBST(&Node{
+		v: 33,
+		left: &Node{
+			v: 16,
+			left: &Node{
+				v: 13,
+				right: &Node{
+					v: 15,
+				},
+			},
+			right: &Node{
+				v: 18,
+				left: &Node{
+					v: 17,
+				},
+				right: &Node{
+					v: 25,
+					left: &Node{
+						v: 19,
+					},
+					right: &Node{
+						v: 27,
+					},
+				},
+			},
+		},
+		right: &Node{
+			v: 50,
+			left: &Node{
+				v: 34,
+			},
+			right: &Node{
+				v: 58,
+				left: &Node{
+					v: 51,
+				},
+				right: &Node{
+					v: 66,
+				},
+			},
+		},
+	})
+}
+
+func (s *BSTTestSuite) TestSearch() {
+	found, isLeftOf, parent := s.bst.Search(19)
+	assert.Equal(s.T(), 19, found.v)
+	assert.Equal(s.T(), true, isLeftOf)
+	assert.Equal(s.T(), 25, parent.v)
+}
+
+func (s *BSTTestSuite) TestInsert() {
+	ok := s.bst.Insert(55)
+	assert.Equal(s.T(), true, ok)
+	found, isLeftOf, parent := s.bst.Search(19)
+	assert.Equal(s.T(), 55, found.v)
+	assert.Equal(s.T(), false, isLeftOf)
+	assert.Equal(s.T(), 51, parent.v)
+}
+
+func (s *BSTTestSuite) TestRemove() {
+	ok := s.bst.Insert(55)
+	ok = s.bst.Remove(55)
+	assert.Equal(s.T(), true, ok)
+	found, isLeftOf, parent := s.bst.Search(55)
+	assert.Nil(s.T(), found)
+	assert.Equal(s.T(), false, isLeftOf)
+	assert.Nil(s.T(), parent)
+
+	ok = s.bst.Remove(13)
+	assert.Equal(s.T(), true, ok)
+	found, isLeftOf, parent = s.bst.Search(13)
+	assert.Nil(s.T(), found)
+	assert.Equal(s.T(), false, isLeftOf)
+	assert.Nil(s.T(), parent)
+	found, _, _ = s.bst.Search(16)
+	assert.Equal(s.T(), 15, found.left.v)
+
+	ok = s.bst.Remove(18)
+	assert.Equal(s.T(), true, ok)
+	found, isLeftOf, parent = s.bst.Search(18)
+	assert.Nil(s.T(), found)
+	assert.Equal(s.T(), false, isLeftOf)
+	assert.Nil(s.T(), parent)
+	found, _, _ = s.bst.Search(19)
+	assert.Equal(s.T(), 17, found.left.v)
+	assert.Equal(s.T(), 25, found.right.v)
 
 }
 
